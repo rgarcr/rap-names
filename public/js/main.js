1ebe3d24382bdf2729b0d9a4efd0af9ec7cc6a44
Ruben Garcia <rgarcr09@gmail.com>
2026-02-01 15:31:37 -0500

    Added client js code to search and display rapper birth name.

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,34 @@
-alert("Working")
+
+const btn = document.querySelector(".btn-search");
+btn.addEventListener('click', async (event) => {
+    const searchName = document.querySelector(".rapper_search").value;
+    if (searchName) {
+        const rapperName = await getRapper(searchName)
+        displayBName(rapperName)
+    }
+    else
+        alert('please enter a name')
+})
+
+async function getRapper(name) {
+    try {
+        const resp = await fetch(`http://localhost:8000/api/${name}`)
+        const data = await resp.json();
+        console.log(data);
+        return data.birthName;
+    }
+    catch (err) {
+        console.error(`Error getting name: `, err.message);
+    }
+}
+
+function displayBName(name) {
+    document.querySelector(".birth_Name").innerHTML = `Birth Name: ${name}`;
+}
+
+function resetBName() {
+    document.querySelector(".birth_Name").innerHTML = 'Birth Name:';
+
+}
+
+
